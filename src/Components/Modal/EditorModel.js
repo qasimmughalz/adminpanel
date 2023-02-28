6a614057133c7b7bf9105c80a5eb26e5e5f7e879
uzairmunir <uzair8825@gmail.com>
2023-02-28 23:00:23 +0500

    added rich-text-editor

---
+++ src/Components/Modal/EditorModel.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+export default function EditorModal({
+  showModal,
+  setShowModal,
+  setDescription,
+}) {
+  const [value, setValue] = useState("");
+
+  const handleClick = () => {
+    setDescription(value);
+    setShowModal(false);
+  };
+
+  const modules = {
+    toolbar: [
+      [{ header: ["1", "2", "3", "4", "5", "6", false] }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  };
+
+  const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+  ];
+  return (
+    <>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-2xl font-semibold">Description</h3>
+                  <button
+                    className="p-1 ml-auto bg-red-400 rounded-full flex items-center justify-center border-0 text-black opacity-1 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => handleClick()}
+                  >
+                    <span className=" text-white flex items-center justify-center  h-6 w-6 text-2xl  outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <ReactQuill
+                    theme="snow"
+                    value={value}
+                    onChange={setValue}
+                    modules={modules}
+                    formats={formats}
+                    placeholder={"Start Typing..."}
+                  />
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-4 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="help-btn"
+                    type="button"
+                    onClick={() => handleClick()}
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
--- src/Containers/Pages/FeaturedVendors/Article.js
+++ src/Containers/Pages/FeaturedVendors/Article.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { toast } from 'react-toastify';
-import ArticleImg from '../../../Assets/img/article.png';
-import { addBlog, handleBlogImage } from '../../../Utils/HelperFunctions';
+import React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import ArticleImg from "../../../Assets/img/article.png";
+import { addBlog, handleBlogImage } from "../../../Utils/HelperFunctions";
+import EditorModel from "../../../Components/Modal/EditorModel.js";
 
 const Article = () => {
   const [article, setArticle] = useState({
-    title: '',
-    description: '',
+    title: "",
+    description: "",
   });
+  const [description, setDescription] = useState("Add Description");
   const [file, setFile] = useState(null);
   const [uploadedImage, setUploadedImage] = useState(null);
+  const [showModal, setShowModal] = useState(false);
   const token = useSelector((state) => state?.Auth.user.data.token);
 
   const handleChange = (e) => {
@@ -19,23 +22,24 @@ const Article = () => {
   };
   const handleSubmit = async () => {
     if (
-      article.title.trim === '' ||
-      article.description.trim() === '' ||
+      article.title.trim === "" ||
+      description.trim() === "" ||
       uploadedImage == null
     ) {
-      toast.error('Please Fill Required Fields');
+      toast.error("Please Fill Required Fields");
     } else {
       let formData = {
         title: article.title,
-        description: article.description,
+        description: description,
         image: uploadedImage,
       };
       let data = await addBlog(formData, token);
       console.log(data);
       setArticle({
-        title: '',
-        description: '',
+        title: "",
+        description: "",
       });
+      setDescription("");
       setUploadedImage(null);
       toast.success(data.message);
     }
@@ -47,7 +51,7 @@ const Article = () => {
   const handleUpload = async () => {
     if (file !== null) {
       let formData = new FormData();
-      formData.append('file', file);
+      formData.append("file", file);
       let data = await handleBlogImage(formData, token);
       console.log(data);
       if (data) {
@@ -57,31 +61,31 @@ const Article = () => {
     }
   };
   return (
-    <div className='w-full lg:w-[483px] h-[359px] bg-white m-auto  rounded-[15px] p-4 responsive-inner-container'>
-      <p className='text-base text-[#121212] font-g-bold p-4 line-height-[28px] md:text-[24px]'>
+    <div className="w-full lg:w-[483px] h-[359px] bg-white m-auto  rounded-[15px] p-4 responsive-inner-container">
+      <p className="text-base text-[#121212] font-g-bold p-4 line-height-[28px] md:text-[24px]">
         Article
       </p>
-      <div className='w-[162px] h-[98px] bg-[#F2F2F2] rounded-lg py-2 flex flex-col items-center justify-center'>
+      <div className="w-[162px] h-[98px] bg-[#F2F2F2] rounded-lg py-2 flex flex-col items-center justify-center">
         <label
-          htmlFor='file'
-          className=' text-gray-500 cursor-pointer mb-2 flex items-center justify-center'
+          htmlFor="file"
+          className=" text-gray-500 cursor-pointer mb-2 flex items-center justify-center"
         >
           <img
             src={uploadedImage !== null ? uploadedImage : ArticleImg}
-            className='w-[44.69px] h-[37.81px] flex items-center justify-center'
+            className="w-[44.69px] h-[37.81px] flex items-center justify-center"
           />
         </label>
         <input
-          type='file'
-          name='file'
-          id='file'
+          type="file"
+          name="file"
+          id="file"
           onChange={handleFile}
-          className='file-input w-full h-[35px] lg:w-[988px]  rounded-[5px]  font-g-medium text-base line-height-[19px] text-[#A2A2A2] mt-2 focus:outline-blue-500 responsive-container'
+          className="file-input w-full h-[35px] lg:w-[988px]  rounded-[5px]  font-g-medium text-base line-height-[19px] text-[#A2A2A2] mt-2 focus:outline-blue-500 responsive-container"
         />
 
         <button
           className={`w-[77px] h-[31px] bg-primary text-white rounded-[5px] mt-2 text-[15px] block mx-auto font-g-regular line-height-[18px] ${
-            file !== null ? 'block' : 'hidden'
+            file !== null ? "block" : "hidden"
           }`}
           onClick={() => handleUpload()}
         >
@@ -89,30 +93,36 @@ const Article = () => {
         </button>
       </div>
       <input
-        type={'text'}
-        name='title'
-        id='title'
+        type={"text"}
+        name="title"
+        id="title"
         value={article.title}
         onChange={handleChange}
-        placeholder='Text'
-        className='w-full h-[32px] lg:w-[432px] border border-[#A2A2A2] rounded-[5px] px-4 font-g-medium text-base line-height-[19px] text-[#A2A2A2] mt-2 focus:outline-blue-500 responsive-container'
+        placeholder="Text"
+        className="w-full h-[32px] lg:w-[432px] border border-[#A2A2A2] rounded-[5px] px-4 font-g-medium text-base line-height-[19px] text-[#A2A2A2] mt-2 focus:outline-blue-500 responsive-container"
       />
-      <input
-        type={'text'}
-        name='description'
-        id='description'
-        value={article.description}
-        onChange={handleChange}
-        placeholder='Description'
-        className='w-full h-[89px] lg:w-[432px] border border-[#A2A2A2] rounded-[5px] px-4 font-g-medium text-base line-height-[19px] text-[#A2A2A2] mt-2 focus:outline-blue-500 responsive-container'
+      <p
+        // onChange={handleChange}
+        readOnly={true}
+        onClick={() => setShowModal(true)}
+        dangerouslySetInnerHTML={{ __html: description }}
+        className="w-full h-[89px] overflow-hidden lg:w-[432px] cursor-pointer border border-[#A2A2A2] rounded-[5px] px-4 font-g-medium text-base line-height-[19px] text-[#A2A2A2] mt-2 focus:outline-blue-500 responsive-container"
       />
 
       <button
-        className='w-[77px] h-[31px] bg-primary text-white rounded-[5px] mt-2 text-[15px] font-g-regular line-height-[18px]'
+        className="w-[77px] h-[31px] bg-primary text-white rounded-[5px] mt-2 text-[15px] font-g-regular line-height-[18px]"
         onClick={() => handleSubmit()}
       >
         Submit
       </button>
+
+      {showModal ? (
+        <EditorModel
+          showModal={showModal}
+          setShowModal={setShowModal}
+          setDescription={setDescription}
+        />
+      ) : null}
     </div>
   );
 };
